fcca7c1f0f53c0e6767f652dd6a82272efea7858
AurelienChetot <aurelien.chetot@gmail.com>
2024-03-18 23:13:48 +0100

    Ajout d'image svg et changement de style

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const theme = document.querySelector(".theme");
-const main = document.querySelector("main");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const info = document.querySelector(".info");
+const title1 = document.querySelector(".title1");
+const p1 = document.querySelector(".imgdev-container");
 
 theme.addEventListener('click', () => {
-    main.classList.toggle('dark')
+    header.classList.toggle('lightheader');
+    footer.classList.toggle('lightheader');
+
+});
+
+theme.addEventListener('click', () => {
+    info.classList.toggle('lightinfo');
+});
+
+theme.addEventListener('click', () => {
+    title1.classList.toggle('lighttitle');
+});
+
+theme.addEventListener('click', () => {
+    p1.classList.toggle('lightp');
 });
